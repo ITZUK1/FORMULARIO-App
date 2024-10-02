36c445af4838fda3714274072333ea2cbd149212
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-10-02 10:20:12 -0500

    se creo la structura html

--- formApp/src/app/reactive/reactive.module.ts
+++ formApp/src/app/reactive/reactive.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
+import { BasicPageComponent } from './pages/basic-page/basic-page.component';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasicPageComponent,
+    DynamicPageComponent
+  ],
   imports: [
     CommonModule,
     ReactiveRoutingModule
