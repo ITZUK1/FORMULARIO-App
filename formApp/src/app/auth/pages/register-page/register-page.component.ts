9cabf8cef76396cf6d2cf959f09ab31612d1162e
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-10-07 11:58:19 -0500

    funciona el registro

--- formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   ]
 })
 export class RegisterPageComponent {
+
+public myForm: FormGroup = this.fb.group({
+  name:['',[Validators.required]],
+  email:['',[Validators.required]],
+  usermame:['',[Validators.required]],
+  password:['',[Validators.required,Validators.minLength(6)]],
+  password2:['',[Validators.required]],
+
+})
+
+constructor (private fb: FormBuilder){}
+
+isValidField(field:string){
+  //TODO: Obtener validacion desde un servicio
+}
+
+onSubmit(){
+  this.myForm.markAllAsTouched();
+}
+
+
+
+
 
 }
